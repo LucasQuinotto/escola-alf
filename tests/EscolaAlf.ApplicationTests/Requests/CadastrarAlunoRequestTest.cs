2913acbef82c0e33f89e4feb2014be8d1bc5ba60
LucasQuinotto <lucas.quinotto.oficial@gmail.com>
2021-01-24 01:20:08 -0300

    Adicionado testes de cadastro de alunos e provas

--- tests/EscolaAlf.ApplicationTests/Requests/CadastrarAlunoRequestTest.cs
+++ tests/EscolaAlf.ApplicationTests/Requests/CadastrarAlunoRequestTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using EscolaAlf.Application.Bean;
 using EscolaAlf.Application.Dtos;
 using EscolaAlf.Application.Entities;
 using EscolaAlf.Application.Requests;
@@ -8,16 +10,64 @@ namespace EscolaAlf.ApplicationTests.Requests
 {
     public class CadastrarAlunoRequestTest
     {
-        [Fact]
-        public void Test1()
+        public CadastrarAlunoRequestTest()
         {
-            var alunoDto = new CadastrarAlunoDto();
+            Dados.Alunos.Clear();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Deve_ocorrer_exception_com_campo_nome_nulo_ou_vazio(string campoNome)
+        {
+            var alunoDto = new CadastrarAlunoDto {Nome = campoNome};
             var request = new CadastrarAlunoRequest();
 
             Func<Aluno> func = () => request.Executar(alunoDto);
 
             Assert.Throws<Exception>(func);
-            Assert.Equal("Campo nome não pode ser branco ou nulo", Assert.Throws<Exception>(func).Message);
+            Assert.Equal(
+                "Campo nome não pode ser branco ou nulo",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(0, Dados.Alunos.Count);
+        }
+
+        [Fact]
+        public void Deve_ocorrer_exception_caso_haja_mais_de_cem_alunos_cadastrado()
+        {
+            var alunoDto = new CadastrarAlunoDto {Nome = "Aluno Teste"};
+            var request = new CadastrarAlunoRequest();
+
+            Enumerable.Range(1, 100).ToList()
+                .ForEach(x => Dados.Alunos.Add(new Aluno
+                {
+                    Id = $"{x}",
+                    Nome = $"Aluno Teste {x}"
+                }));
+
+            Func<Aluno> func = () => request.Executar(alunoDto);
+
+            Assert.Throws<Exception>(func);
+            Assert.Equal(
+                "Limite máximo de 100 alunos cadastrados foi atingido",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(100, Dados.Alunos.Count);
+        }
+
+        [Fact]
+        public void Deve_cadastrar_aluno()
+        {
+            var alunoDto = new CadastrarAlunoDto {Nome = "Aluno Teste"};
+            var request = new CadastrarAlunoRequest();
+
+            var aluno = request.Executar(alunoDto);
+
+            Assert.NotNull(aluno.Id);
+            Assert.NotEmpty(aluno.Id);
+            Assert.Equal("ALUNO TESTE", aluno.Nome);
+            Assert.Equal(1, Dados.Alunos.Count);
         }
     }
 }
---
+++ tests/EscolaAlf.ApplicationTests/Requests/CadastrarProvaRequestTest.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using EscolaAlf.Application.Bean;
+using EscolaAlf.Application.Dtos;
+using EscolaAlf.Application.Entities;
+using EscolaAlf.Application.Requests;
+using Xunit;
+
+namespace EscolaAlf.ApplicationTests.Requests
+{
+    public class CadastrarProvaRequestTest
+    {
+        public CadastrarProvaRequestTest()
+        {
+            Dados.Provas.Clear();
+        }
+
+        [Fact]
+        public void Deve_ocorrer_exception_com_peso_zero()
+        {
+            var provaDto = new CadastrarProvaDto
+            {
+                Questoes = new List<QuestaoDto>
+                {
+                    new QuestaoDto {Numero = 1, Peso = 0},
+                }
+            };
+            var request = new CadastrarProvaRequest();
+
+            Func<Prova> func = () => request.Executar(provaDto);
+
+            Assert.Throws<Exception>(func);
+            Assert.Equal(
+                "Peso da questão não pode ser zero",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(0, Dados.Provas.Count);
+        }
+
+        [Fact]
+        public void Deve_ocorrer_exception_caso_haja_questoes_duplicadas()
+        {
+            var provaDto = new CadastrarProvaDto
+            {
+                Questoes = new List<QuestaoDto>
+                {
+                    new QuestaoDto {Numero = 1, Peso = 1},
+                    new QuestaoDto {Numero = 1, Peso = 2},
+                }
+            };
+            var request = new CadastrarProvaRequest();
+
+            Func<Prova> func = () => request.Executar(provaDto);
+
+            Assert.Throws<Exception>(func);
+            Assert.Equal(
+                "Numero da questão não pode ser duplicado",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(0, Dados.Provas.Count);
+        }
+
+        [Fact]
+        public void Deve_ocorrer_exception_caso_sequencia_de_questoes_nao_comece_em_1()
+        {
+            var provaDto = new CadastrarProvaDto
+            {
+                Questoes = new List<QuestaoDto>
+                {
+                    new QuestaoDto {Numero = 0, Peso = 1},
+                    new QuestaoDto {Numero = 1, Peso = 2},
+                }
+            };
+            var request = new CadastrarProvaRequest();
+
+            Func<Prova> func = () => request.Executar(provaDto);
+
+            Assert.Throws<Exception>(func);
+            Assert.Equal(
+                "A sequencia de questões deve começar em 1",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(0, Dados.Provas.Count);
+        }
+
+        [Fact]
+        public void Deve_ocorrer_exception_caso_sequencia_de_questoes_nao_seja_completa()
+        {
+            var provaDto = new CadastrarProvaDto
+            {
+                Questoes = new List<QuestaoDto>
+                {
+                    new QuestaoDto {Numero = 1, Peso = 1},
+                    new QuestaoDto {Numero = 2, Peso = 2},
+                    new QuestaoDto {Numero = 4, Peso = 3},
+                }
+            };
+            var request = new CadastrarProvaRequest();
+
+            Func<Prova> func = () => request.Executar(provaDto);
+
+            Assert.Throws<Exception>(func);
+            Assert.Equal(
+                "Nem todos os numeros de questões foram encontrados",
+                Assert.Throws<Exception>(func).Message
+            );
+            Assert.Equal(0, Dados.Provas.Count);
+        }
+
+        [Fact]
+        public void Deve_cadastrar_prova_com_sucesso()
+        {
+            var provaDto = new CadastrarProvaDto
+            {
+                Questoes = new List<QuestaoDto>
+                {
+                    new QuestaoDto {Numero = 1, Peso = 1},
+                    new QuestaoDto {Numero = 2, Peso = 2},
+                    new QuestaoDto {Numero = 3, Peso = 3},
+                    new QuestaoDto {Numero = 4, Peso = 1},
+                }
+            };
+            var request = new CadastrarProvaRequest();
+
+            var prova = request.Executar(provaDto);
+
+            Assert.Contains("ALF-", prova.Id);
+            Assert.Equal(4, prova.Questoes.Count);
+            Assert.Equal(1, Dados.Provas.Count);
+        }
+    }
+}
